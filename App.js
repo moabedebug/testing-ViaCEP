33d4df1d90e7d45941f86296974b013988158eee
Moabe <moabeluis2018@gmail.com>
2022-07-14 17:09:37 -0300

    finalizado

--- App.js
+++ App.js
@@ -1,48 +1,91 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react"
 import {
   View,
   Text,
   TextInput,
   TouchableOpacity,
   StyleSheet,
-  SafeAreaView
+  SafeAreaView,
+  Keyboard
 } from "react-native"
 
 import api from "./src/services/api"
 
+  
+
 export default function App() {
 
   const [cep, setCep] = useState("")
+  const [cepUser, setCepUser] = useState(null)
+  const inputRef = useRef(null)
+
+  async function buscar(){
+    if(cep == "") {
+      alert("Digite um Cep Valido")
+      
+      return
+    }
+    
+    try{
+      const response = await api.get(`/${cep}/json`)
+      console.log(response.data)
+      setCepUser(response.data)
+
+      Keyboard.dismiss()
+    }catch(error){
+      console.log("ERROR: " + error)
+    }
+
+    
+
+  }
+
+  function limpar(){
+    setCep("")
+    inputRef.current.focus()
+    setCepUser(null)
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.subContainer}>
-        <Text style={styles.text}>Digite seu CEP desejado</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Ex: 45000000"
-          value={cep}
-          onChangeText={ (texto) => setCep(texto)}
-          keyboardType="numeric"
-        />
-      </View>
+        <View style={styles.subContainer}>
+          <Text style={styles.text}>Digite seu CEP desejado</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Ex: 45000000"
+            value={cep}
+            onChangeText={ (texto) => setCep(texto)}
+            keyboardType="numeric"
+            ref={inputRef}
+          />
+        </View>
 
-      <View style={styles.areaBtn}>
-        <TouchableOpacity style={[styles.btn, { backgroundColor : "#1d75cd" }]}>
-          <Text style={styles.textButton}>Buscar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.btn, { backgroundColor : "#cd3e1d" }]}>
-          <Text style={styles.textButton}>Limpar</Text>
-        </TouchableOpacity>
-      </View>
+        <View style={styles.areaBtn}>
+          <TouchableOpacity 
+            style={[styles.btn, { backgroundColor : "#1d75cd" }]}
+            onPress={buscar}
+            >
+            <Text style={styles.textButton}>Buscar</Text>
+          </TouchableOpacity>
 
-      <View style={styles.result}>
-        <Text style={styles.itemText}>CEP: </Text>
-        <Text style={styles.itemText}>Rua: </Text>
-        <Text style={styles.itemText}>Bairro: </Text>
-        <Text style={styles.itemText}>Cidade: </Text>
-        <Text style={styles.itemText}>Estado: </Text>
-      </View>
+          <TouchableOpacity 
+            style={[styles.btn, { backgroundColor : "#cd3e1d" }]}
+            onPress={ limpar }
+          >
+            <Text style={styles.textButton}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+
+        { cepUser &&  
+          <View style={styles.result}>
+            <Text style={styles.itemText}>CEP: {cepUser.cep}</Text>
+            <Text style={styles.itemText}>Rua: {cepUser.logradouro}</Text>
+            <Text style={styles.itemText}>Bairro: {cepUser.bairro}</Text>
+            <Text style={styles.itemText}>Cidade: {cepUser.localidade}</Text>
+            <Text style={styles.itemText}>Estado: {cepUser.uf}</Text>
+          </View>
+        }
+        
     </SafeAreaView>
   )
 }
